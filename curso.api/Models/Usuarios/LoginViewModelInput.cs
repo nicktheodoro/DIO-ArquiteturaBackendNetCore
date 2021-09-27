a1ac695033cda3e414609f0d301647dd4f8ae50d
nicktheodoro <nicolastheodoro@gmail.com>
2021-09-27 17:53:45 -0300

    Módulo 01 - Arquitetura backend com .NET Core

---
+++ curso.api/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace curso.api.Models.Usuarios
+{
+    public class LoginViewModelInput
+    {
+        [Required(ErrorMessage = "O campo login é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatória")]
+        public string Senha { get; set; }
+
+    }
+}
---
+++ curso.api/Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models.Usuarios
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O login é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
